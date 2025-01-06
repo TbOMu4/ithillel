4fe3ab29626cc16b7f06c325c243b788e77e512b
TbOMu4 <artemtumoshenko@gmail.com>
2025-01-06 23:46:54 +0200

    homework

--- homework-3.3/js/index.js
+++ homework-3.3/js/index.js
@@ -2,6 +2,6 @@
 
 const number =10369
 const numberString = number.toString()
-const numberArrayToString = numberString.split('').join(' ')
+const numberArrayToString = numberString.split(' ').join(' ')
 console.log(numberArrayToString)
 
