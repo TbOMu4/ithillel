88a8246c03f3c43447f47bd50c136028e209ea36
TbOMu4 <artemtumoshenko@gmail.com>
2025-01-07 23:27:53 +0200

    homework

---
+++ homework-4.2/js/index.js
@@ -0,0 +1,31 @@
+"use strict"
+
+const number = parseInt(prompt('Enter a three-digit integer', '0'))
+const checkThreeDigitNumber = /^\d{3}$/
+
+if (checkThreeDigitNumber.test(number.toString())) {
+  const numberString = number.toString()
+  let allDigitsSame = true
+
+  for (let i = 1; i < numberString.length; i++) {
+    if (numberString[i] !== numberString[0]) {
+      allDigitsSame = false
+      break
+    }
+  }
+
+  if (allDigitsSame) {
+    alert('All digits are the same')
+  } else {
+    alert('Not all digits are the same')
+    const uniqueDigits = new Set(numberString)
+    if (uniqueDigits.size < numberString.length) {
+      alert('There are repeated digits in the number')
+    } else {
+      alert('All digits are unique')
+    }
+  }
+
+} else {
+  alert('You did not enter a three-digit number')
+}
---
+++ homework-4.3/js/index.js
@@ -0,0 +1,60 @@
+"use strict"
+
+const userYearBirth = parseInt(prompt("Введіть свій рік народження:", "0"))
+const currentYear = new Date().getFullYear();
+
+if (userYearBirth > 0 && userYearBirth <= currentYear) {
+  const ageUser = currentYear - userYearBirth;
+  alert(`Ваш вік ${ageUser}`)
+} else {
+  alert(`Шкода, що ви не захотіли вписати свій справжній рік народження`)
+}
+
+const userCityResidence = prompt("Введіть своє місто проживання: ") ?? ""
+let messageFirst
+let userCountry
+
+if (userCityResidence.trim()) {
+  switch (userCityResidence.toLowerCase()) {
+    case "київ":
+      userCountry = "України"
+      messageFirst = `Ти живеш у столиці ${userCountry}`
+      break
+    case "вашингтон":
+      userCountry = "США"
+      messageFirst = `Ти живеш у столиці ${userCountry}`
+      break
+    case "лондон":
+      userCountry = "Велика Британії"
+      messageFirst = `Ти живеш у столиці ${userCountry}`
+      break
+    default:
+      messageFirst = `Ти живеш у місті ${userCityResidence}`
+  }
+  alert(messageFirst)
+} else {
+  alert(`Шкода, що ви не захотіли вказати місто проживання`)
+}
+
+const userFavoriteSport = prompt("Введіть свій улюблений вид спорту: ") ?? ""
+let messageSecond
+
+if (userFavoriteSport.trim()) {
+  switch (userFavoriteSport.toLowerCase()) {
+    case "Бамбінтон":
+      messageSecond = `Круто! Хочеш стати як Ли Ян Ван Цзилинь?`
+      break
+    case "Бокс":
+      messageSecond = `Круто! Хочеш стати як  Олександр Усик?`
+      break
+    case "Футбол":
+      messageSecond = `Круто! Хочеш стати як Гаррі Магвайр?`
+      break
+    default:
+      messageSecond = `Гарний вибір!`;
+  }
+  alert(messageSecond)
+  alert(`Ваш улюблений вид спорту ${userFavoriteSport}`)
+} else {
+  alert(`Шкода, що ви не захотіли вказати улюблений вид спорту`)
+}
