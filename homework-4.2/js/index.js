0aa9cb55c51dc1278d572785976f83771ba75d7a
TbOMu4 <artemtumoshenko@gmail.com>
2025-01-09 12:38:05 +0200

    homework

--- homework-4.2/js/index.js
+++ homework-4.2/js/index.js
@@ -1,31 +1,45 @@
 "use strict"
 
-const number = parseInt(prompt('Enter a three-digit integer', '0'))
+const number =  prompt('Enter a three-digit integer', '0')
 const checkThreeDigitNumber = /^\d{3}$/
 
-if (checkThreeDigitNumber.test(number.toString())) {
-  const numberString = number.toString()
-  let allDigitsSame = true
+if (checkThreeDigitNumber.test(number)) {
+  const uniqueDigits = new Set(number)
 
-  for (let i = 1; i < numberString.length; i++) {
-    if (numberString[i] !== numberString[0]) {
-      allDigitsSame = false
-      break
-    }
-  }
-
-  if (allDigitsSame) {
+  if (uniqueDigits.size === 1) {
     alert('All digits are the same')
+  } else if (uniqueDigits.size < number.length) {
+    alert('There are repeated digits in the number')
   } else {
-    alert('Not all digits are the same')
-    const uniqueDigits = new Set(numberString)
-    if (uniqueDigits.size < numberString.length) {
-      alert('There are repeated digits in the number')
-    } else {
-      alert('All digits are unique')
-    }
+    alert('All digits are unique')
   }
-
 } else {
   alert('You did not enter a three-digit number')
 }
+
+// if (checkThreeDigitNumber.test(number.toString())) {
+//   const numberString = number.toString()
+//   let allDigitsSame = true
+//
+//   for (let i = 1; i < numberString.length; i++) {
+//     if (numberString[i] !== numberString[0]) {
+//       allDigitsSame = false
+//       break
+//     }
+//   }
+//
+//   if (allDigitsSame) {
+//     alert('All digits are the same')
+//   } else {
+//     alert('Not all digits are the same')
+//     const uniqueDigits = new Set(numberString)
+//     if (uniqueDigits.size < numberString.length) {
+//       alert('There are repeated digits in the number')
+//     } else {
+//       alert('All digits are unique')
+//     }
+//   }
+//
+// } else {
+//   alert('You did not enter a three-digit number')
+// }
