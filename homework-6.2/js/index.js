9877ca687f192e9edd12e3756dd4905238454c2e
TbOMu4 <artemtumoshenko@gmail.com>
2025-01-18 14:53:53 +0200

    homework

--- homework-6.2/js/index.js
+++ homework-6.2/js/index.js
@@ -2,7 +2,7 @@
 
 const arrayMixed = [42, "Привіт, світ!", 3.14, true, null, [1, 72, 38], 55, 444, {name: "Artem", age: 33}]
 
-const calculateArithmeticMean = (array) => {
+const calculateArithmeticMeanFirst = (array) => {
   const numericalValue = array.flat().filter(item => typeof item === 'number')
   if (numericalValue.length === 0) {
     return `В даном масиві [${numericalValue}] відсутні числові елементи`
@@ -15,4 +15,11 @@ const calculateArithmeticMean = (array) => {
   return `Середне арефметичне масива = ${arithmeticMean}`
 }
 
-console.log(calculateArithmeticMean(arrayMixed))
+const calculateArithmeticMeanSecond = (array) => array.flat()
+    .filter(item => typeof item === 'number')
+    .reduce((prevValue, currentValue, _, filteredArray) =>
+        prevValue + currentValue / filteredArray.length, 0)
+
+
+console.log(calculateArithmeticMeanFirst(arrayMixed))
+console.log(`Середне арефметичне масива = ${calculateArithmeticMeanSecond(arrayMixed) === 0 ? 'В даном масиві відсутні числові елементи': calculateArithmeticMeanSecond(arrayMixed) }`)
--- homework-6.3/js/index.js
+++ homework-6.3/js/index.js
@@ -3,7 +3,7 @@
 const array = [4, 5, 8, 7, 45, 6, 77, 88, 55, 2, 14, 4]
 console.log(array)
 const removeElement = (array, item) => array.filter(el => el !== item)
-console.log(removeElement(array, 4))//[5, 8, 7, 45, 6, 77, 88, 55, 2, 14]
+console.log(removeElement(array, 4))
 
 console.log(`Початковий масив: ${array} 
-Змінений масив:     ${removeElement(array, 4)}`)//[5, 8, 7, 45, 6, 77, 88, 55, 2, 14]
+Змінений масив:     ${removeElement(array, 4)}`)
