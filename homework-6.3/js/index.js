3b4b9ceb3b8c6f36e643a31148ef2178cd0539c7
TbOMu4 <artemtumoshenko@gmail.com>
2025-01-18 14:23:37 +0200

    homework

--- homework-6.3/js/index.js
+++ homework-6.3/js/index.js
@@ -1,6 +1,9 @@
 "use strict"
 
 const array = [4, 5, 8, 7, 45, 6, 77, 88, 55, 2, 14, 4]
-
+console.log(array)
 const removeElement = (array, item) => array.filter(el => el !== item)
-console.log(removeElement(array,4))
+console.log(removeElement(array, 4))//[5, 8, 7, 45, 6, 77, 88, 55, 2, 14]
+
+console.log(`Початковий масив: ${array} 
+Змінений масив:     ${removeElement(array, 4)}`)//[5, 8, 7, 45, 6, 77, 88, 55, 2, 14]
