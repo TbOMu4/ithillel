1657559287d700b7130179976129dad8af5e538b
TbOMu4 <artemtumoshenko@gmail.com>
2025-02-07 16:41:54 +0200

    homework

--- homework-10.1/js/index.js
+++ homework-10.1/js/index.js
@@ -4,13 +4,29 @@ let user = {
     name: "Artem",
     age: 33,
     city: "Kyiv",
-    country: "Ukraine"
+    country: "Ukraine",
+
+    getUserInfoFirst: function () {
+        const {name, age, city, country} = user
+        document.write(`Моє ім'я ${name}, мені ${age} років, я живу в місті ${city}, яке є столицею ${country}. <br>`)
+    },
+
+    getUserInfoSecond: function () {
+        document.write(`Моє ім'я ${this.name}, мені ${this.age} років, я живу в місті ${this.city}, яке є столицею ${this.country}. <br>`)
+    },
+
+    getUserInfoThird: function (...info) {
+        let userInfo = ``
+        if (info.includes('name')) userInfo += `Моє ім'я ${this.name},`
+        if (info.includes('age')) userInfo += ` мені ${this.age} років,`
+        if (info.includes("city")) userInfo += ` я живу в місті ${this.city},`
+        if (info.includes("country")) userInfo += ` яке є столицею ${this.country},`
+        userInfo = userInfo.trim().replace(/,$/, '.')
+        document.write(userInfo)
+    }
 }
 
-const {name, age, city, country} = user
+user.getUserInfoFirst()
+user.getUserInfoSecond()
+user.getUserInfoThird('name', 'age')
 
-document.write(`Моє ім'я ${name}, мені ${age} років, я живу в місті ${city}, яке є столицею ${country}.`)
-
-Object.entries(user).forEach(([key, value]) => {
-    console.log(`${key}: ${value}`)
-})
