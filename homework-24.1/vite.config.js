1e1d79fd3995f8008c96f0a4e10459b08dc096cb
TbOMu4 <artemtumoshenko@gmail.com>
2025-04-08 21:37:49 +0300

    homework

--- homework-24.1/vite.config.js
+++ homework-24.1/vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
+// https://vite.dev/config/
 export default defineConfig({
-  base: "/ithillel/homework-24.1/index.html", // 👈 дуже важливо
   plugins: [react(), tailwindcss()],
 });
