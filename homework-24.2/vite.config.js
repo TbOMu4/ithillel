1f0a3beed0bcb085392495a492f5d86b6db03a58
TbOMu4 <artemtumoshenko@gmail.com>
2025-04-08 23:28:04 +0300

    homeork

--- homework-24.2/vite.config.js
+++ homework-24.2/vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/ithillel/homework-24.2/dist/index.html",
+  base: "/ithillel/homework-24.2/index.html",
   plugins: [react(), tailwindcss()],
 });
