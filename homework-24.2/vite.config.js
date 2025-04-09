5a130b25a8edb665b425330783beb6acfc89c600
TbOMu4 <artemtumoshenko@gmail.com>
2025-04-09 16:05:40 +0300

    homework

--- homework-24.2/vite.config.js
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite";
-export default defineConfig({
-  base: "/ithillel/homework-24.2",
-  plugins: [react(), tailwindcss()],
-});
